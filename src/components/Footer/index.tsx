b209102f1117b4ce3d5afe3eca8fc4c8bb0b5382
Madera <MaderaHLN@hotmail.com>
2022-10-30 08:01:16 -0600

    add: footer modified and added some styles

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,7 @@
 // Dependencies
 import { Grid } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import Link from 'next/link';
 
 // Styles
 import styles from './styles.module.scss';
@@ -7,6 +9,20 @@ import styles from './styles.module.scss';
 export const Footer = (): JSX.Element => (
   <footer>
     <Grid container className={styles.footer}>
+      <Grid item xs={12} sm={10} md={6} className={styles.footer__info}>
+        <div>
+          <p>
+            Made by <strong>Abisai Antonio Madera</strong>
+          </p>
+        </div>
+        <a
+          href="https://github.com/Madera971003/pokemons-next-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <GitHubIcon />
+        </a>
+      </Grid>
       <Grid item xs={12} sm={10} md={6} className={styles.footer__rights}>
         <div>
           <p>
